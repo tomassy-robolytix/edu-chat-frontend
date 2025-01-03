ae9d068593a037ffa8b8cc383b55aff657a7c677
tomassy-robolytix <79094347+tomassy-robolytix@users.noreply.github.com>
2025-01-03 14:03:22 +0100

    Update Learn.js

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+const Learn = () => {
+  return (
+    <div>
+      <h1>Learning Environment</h1>
+      <p>Here will be the AI teaching system.</p>
+    </div>
+  );
+};
+
+export default Learn;
 
